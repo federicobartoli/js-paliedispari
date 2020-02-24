f773e113730e6fea881f1b50085284379bb667df
Federico <federicobartoli@me.com>
2020-02-24 17:34:56 +0100

    Fine esercizio | messo check anche per il numero

--- ex2/js/main.js
+++ ex2/js/main.js
@@ -12,33 +12,37 @@ sceltaPariODispariChecked = LwCase(sceltaPariODispari)
 
 if (sceltaPariODispariChecked == ('pari') || sceltaPariODispariChecked == ('dispari')) {
      var acquisiscoNumero = parseInt(prompt('Scegli un numero da 1 a 10'));
-     if (acquisiscoNumero > 10) {
-          document.getElementById('id').innerHTML = 'Spiacente hai scelto un numero superiore di 10 fai refresh e riprova';
+     if (!isNaN(acquisiscoNumero)) {
+          if (acquisiscoNumero > 10) {
+               document.getElementById('id').innerHTML = 'Spiacente hai scelto un numero superiore di 10 fai refresh e riprova';
+          } else {
+               var numeroComputer = Math.floor(Math.random() * 10) + 1;
+               // console.log(numeroComputer); // DEBUG
+               document.getElementById('id').innerHTML = 'Il tuo numero è : ' + acquisiscoNumero + ' ; ' + 'il numero del computer è : ' + numeroComputer;
+               var numeroSommato = numeroComputer + acquisiscoNumero;
+               // console.log(numeroSommato); // DEBUG
+               document.getElementById('id-2').innerHTML = 'La somma dei numeri è : ' + numeroSommato;
+               var numeroPari = isPari(numeroSommato)
+               if (sceltaPariODispariChecked == ('pari')) {
+                    if (numeroPari) {
+                         document.getElementById('id-3').innerHTML = 'Complimenti hai scelto pari e il numero sommato è pari..hai vinto un viaggio alle maldive il 31 Febbraio, contatta federicobartoli@me.com per info!!!!!';
+
+                    } else {
+                         document.getElementById('id-3').innerHTML = 'Hai scelto pari ma il numero sommato è dispari..sorry...';
+
+                    }
+               } else if(sceltaPariODispariChecked == ('dispari')) {
+                    if (numeroPari) {
+                         document.getElementById('id-3').innerHTML = 'Hai scelto dispari ma il numero sommato è pari! Non hai vinto sorry';
+
+                    } else {
+                         document.getElementById('id-3').innerHTML = 'Complimenti hai scelto dispari e il numero sommato è dispari..hai vinto un viaggio alle maldive il 31 Febbraio, contatta federicobartoli@me.com per info!!!!!';
+
+                    }
+               }
+          }
      } else {
-          var numeroComputer = Math.floor(Math.random() * 10) + 1;
-          // console.log(numeroComputer); // DEBUG
-          document.getElementById('id').innerHTML = 'Il tuo numero è : ' + acquisiscoNumero + ' ; ' + 'il numero del computer è : ' + numeroComputer;
-          var numeroSommato = numeroComputer + acquisiscoNumero;
-          // console.log(numeroSommato); // DEBUG
-          document.getElementById('id-2').innerHTML = 'La somma dei numeri è : ' + numeroSommato;
-          var numeroPari = isPari(numeroSommato)
-          if (sceltaPariODispariChecked == ('pari')) {
-               if (numeroPari) {
-                    document.getElementById('id-3').innerHTML = 'Complimenti hai scelto pari e il numero sommato è pari..hai vinto un viaggio alle maldive il 31 Febbraio, contatta federicobartoli@me.com per info!!!!!';
-
-               } else {
-                    document.getElementById('id-3').innerHTML = 'Hai scelto pari ma il numero sommato è dispari..sorry...';
-
-               }
-          } else if(sceltaPariODispariChecked == ('dispari')) {
-               if (numeroPari) {
-                    document.getElementById('id-3').innerHTML = 'Hai scelto dispari ma il numero sommato è pari! Non hai vinto sorry';
-
-               } else {
-                    document.getElementById('id-3').innerHTML = 'Complimenti hai scelto dispari e il numero sommato è dispari..hai vinto un viaggio alle maldive il 31 Febbraio, contatta federicobartoli@me.com per info!!!!!';
-
-               }
-          } 
+          document.getElementById('id').innerHTML = 'Fai un Refresh e inserisci un valore valido : NUMERI';
      }
 } else {
      document.getElementById('id').innerHTML = 'Fai un Refresh e inserisci un valore valido : Pari o Dispari';
@@ -51,6 +55,14 @@ function LwCase(stringa) {
      var lowStr = stringa.toLowerCase().replace(check, ''); //Trasformo tutto in minuscolo , attacco tutto e tolgo i caratteri speciali.
      return lowStr
 }
+
+
+// function numberTrue(stringa) {
+//      if {
+//           !isNaN(stringa)
+//
+//      }
+// }
 
 function isPari(numero) {
      if(numero % 2 === 0){
