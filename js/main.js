710b15d601a57eafcad6e40d7cf3d164d09402c7
Federico <federicobartoli@me.com>
2020-02-24 16:08:21 +0100

    Esercizio palindromo fatto

--- js/main.js
+++ js/main.js
@@ -1,15 +1,36 @@
-// function palindrome(stringa) {
-//   var re = /[\W_]/g;
-//   var lowRegStr = stringa.toLowerCase().replace(re, '');
-//   var reverseStr = lowRegStr.split('').reverse().join('');
-//   return reverseStr === lowRegStr;
-// }
 
-//PROVE DI CODICE X PALINDROMO
-var ciao = 'ciao';
-var ris = ciao.split(''); //Con split creo un array di tutte le parole divise di ciao
-var rev = ris.reverse(); // con reverse metto al contrario tutte le parole
-var jn = rev.join(''); // con join unisco array e faccio diventare stringa
-console.log(ris);
-console.log(rev);
-console.log(jn);
+
+// //PROVE DI CODICE X PALINDROMO
+// var ciao = 'ciao';
+// var spl = ciao.split(''); //Con split creo un array di tutte le parole divise di ciao
+// var rev = spl.reverse(); // con reverse metto al contrario tutte le parole
+// var joi = rev.join(''); // con join unisco array e faccio diventare stringa
+// console.log(spl);
+// console.log(rev);
+// console.log(joi);
+
+palindromoSi= false;
+var parolaUtente = prompt('inserisci una parola');
+var parolaUtenteCheck = palindromo(parolaUtente);
+console.log(parolaUtenteCheck);
+
+if (palindromoSi) {
+     document.getElementById('id').innerHTML = 'Hai un palindromo'
+     console.log('SI PALINDROMO');
+} else {
+      document.getElementById('id').innerHTML  = 'Non hai un palindromo'
+     console.log('NO PALINDROMO');
+}
+
+function palindromo(stringa) {
+     var check = /[^A-Za-z0-9]/g; //Check lettere e numeri NOCARATTERISPECIALI
+     var lowStr = stringa.toLowerCase().replace(check, ''); //Trasformo tutto in minuscolo , attacco tutto e tolgo i caratteri speciali.
+     var reverseStr = lowStr.split('').reverse().join('');
+     if (reverseStr == lowStr) {
+          return palindromoSi= true;
+     } else {
+          return console.log('no palindromo');
+     }
+
+
+}
