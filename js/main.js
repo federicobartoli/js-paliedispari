77852b6ce0dc184f58f734f4c3eef5b9f643347a
Federico <federicobartoli@me.com>
2020-02-24 15:35:05 +0100

    inizio esercizio e prove

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// function palindrome(stringa) {
+//   var re = /[\W_]/g;
+//   var lowRegStr = stringa.toLowerCase().replace(re, '');
+//   var reverseStr = lowRegStr.split('').reverse().join('');
+//   return reverseStr === lowRegStr;
+// }
+
+
+var ciao = 'ciao';
+var ris = ciao.split(''); //Con split creo un array di tutte le parole divise di ciao
+var rev = ris.reverse(); // con reverse metto al contrario tutte le parole
+var jn = rev.join(''); // con join unisco array e faccio diventare stringa
+console.log(ris);
+console.log(rev);
+console.log(jn);
